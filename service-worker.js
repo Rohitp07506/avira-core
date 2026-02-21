6da5ec5e8b4f02435cdf5774b025e05a80cb361b
Rohitp07506 <rohitp2012@gmail.com>
2026-02-21 22:21:17 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,11 @@
-self.addEventListener("install", () => {
+self.addEventListener("install", event => {
   self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
 });
