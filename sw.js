bbf91c85d802c31a165c82ab650b6a5cc05bbcb8
Rohitp07506 <rohitp2012@gmail.com>
2026-02-21 20:11:19 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open("avira-cache").then(function (cache) {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/style.css",
+        "/main.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
+});
